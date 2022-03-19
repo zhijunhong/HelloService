8087d78a53d9816b524d791515168b705f81e851
zhijunhong <981827802@qq.com>
2022-03-19 20:42:08 +0800

    添加日志系统

--- src/main/java/org/example/controller/IndexController.java
+++ src/main/java/org/example/controller/IndexController.java
@@ -1,5 +1,7 @@
 package org.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping(path = "")
     public String index(){
+        Logger logger = LoggerFactory.getLogger(IndexController.class);
+        //调用 sl4j 的 info() 方法，而非调用 logback 的方法
+        logger.info("Hello World");
+
         return "Hello Spring Boot!";
     }
 }
